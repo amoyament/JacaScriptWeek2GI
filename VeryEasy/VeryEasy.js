b8abc18c9fcb12f5326674d88c5a68b4df86dd40
Aubrey Moya-Mendez Trotter <Aubrey@AdminisatorsMBP.lan>
2023-04-28 20:27:20 -0400

    VeryEasy challenge completed w/ 3 parts

---
+++ VeryEasy/VeryEasy.js
@@ -0,0 +1,55 @@
+// VERY EASY: Write a function named min that takes two arguments and returns their minimum.
+//Create the variables
+
+let num1 = 75;
+let num2 = 5;
+
+//Create a funtion named min that takes two arguments (num1 and num 2)
+let min = function (num1, num2) {
+  //Create an if/else statement to comare the two numbers
+  //If num one is less than num two, it is returned as the minimum
+  if (num1 < num2) {
+    return `The number ${num1} is less than the number ${num2}.`;
+    //If num two is less than num one, it is returned as the minimum
+  } else if (num1 > num2) {
+    return `The number ${num2} is less than the number ${num1}.`;
+    //If neither number is greater or lesser, than they are even
+  } else {
+    return `The numbers ${num1} and ${num2} are even.`;
+  }
+};
+
+// Log the function to make sure it works
+console.log(min(num1, num2));
+
+//This function can also be used with other numbers
+console.log(min(93, 55));
+
+//This can also be written as an arrow function with ternary operators
+let minA = (numA, numB) => {
+  return numA < numB
+    ? `The number ${numA} is less than the number ${numB}.`
+    : numA > numB
+    ? `The number ${numB} is less than the number ${numA}.`
+    : `The numbers ${numA} and ${numB} are even.`;
+};
+
+console.log(minA(43, 66));
+
+//If you want to have more than two numbers, you can write the numbers in an array
+//First we need to store the numbers in an array
+let nums = [-3, 13, 17, 15, 14, -9, 5];
+
+//Now we will create a function named minA, so the program doesn't get confused by our two functions
+let minB = function (nums) {
+  //Set min to the first value in the array, then we will comapre it to the others
+  let min = nums[0];
+  //To compare each value to the next we will create a for loop and use the .length method to loop through the entire array
+  for (let i = 0; i < nums.length; i++) {
+    //Use an if statement to comare the numbers to see which is the lesser and set the lesser as the min
+    if (nums[i] < min) min = nums[i];
+  }
+  return `${min} is the minimum number in this array.`;
+};
+
+console.log(minB(nums));
